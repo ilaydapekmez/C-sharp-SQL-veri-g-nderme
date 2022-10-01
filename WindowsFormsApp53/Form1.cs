4c95572decf4ab63ee76767a20b4c8254e0b30d7
ilaydapekmez <111456821+ilaydapekmez@users.noreply.github.com>
2022-10-01 12:44:00 +0300

    C#

---
+++ WindowsFormsApp53/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace WindowsFormsApp53
+{
+    public partial class Form1 : Form
+    {
+
+        MySqlConnection baglanti;
+        MySqlCommand komut;
+        MySqlDataReader oku;
+        public Form1()
+        {
+            InitializeComponent();
+
+  
+            baglanti = new MySqlConnection("connection string");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+   
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+           
+            komut = new MySqlCommand();
+            komut.Connection = baglanti;
+            komut.CommandText = "INSERT INTO `happy`(`ograd`, `ogrsoyad`, `dtarih`) VALUES('"+textBox1.Text+"', '"+textBox2.Text+"', '"+textBox3.Text+"')";
+            komut.ExecuteNonQuery();
+
+            baglanti.Close();
+            MessageBox.Show("Eklendiii :)) ");
+        }
+    }
+}
